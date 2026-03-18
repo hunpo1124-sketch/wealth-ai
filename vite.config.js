20e4e5531b268a3257e424862b6393e8c752ef45
hunpo1124 <hunpo1124@gmail.com>
2026-03-18 23:28:47 +0800

    chore: adapt for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -19,12 +19,12 @@ export default defineConfig({
         theme_color: '#2D6A4F',
         icons: [
           {
-            src: 'icon-192x192.png',
+            src: '/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'icon-512x512.png',
+            src: '/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
